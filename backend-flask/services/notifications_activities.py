298274871382560353fffd844a9e825d6d4a0d75
Albert I Palacios <114888726+J-Lobster@users.noreply.github.com>
2023-02-20 18:45:17 -0500

    Update notifications_activities.py
    I changed the notification messages and users.

--- backend-flask/services/notifications_activities.py
+++ backend-flask/services/notifications_activities.py
@@ -4,8 +4,8 @@ class NotificationsActivities:
     now = datetime.now(timezone.utc).astimezone()
     results = [{
       'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-      'handle':  'Akalia',
-      'message': 'My man is hot',
+      'handle':  'somegal',
+      'message': 'Wow, I love Velma!',
       'created_at': (now - timedelta(days=2)).isoformat(),
       'expires_at': (now + timedelta(days=5)).isoformat(),
       'likes_count': 5,
@@ -15,7 +15,7 @@ class NotificationsActivities:
         'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea67',
         'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
         'handle':  'someguy',
-        'message': "You're not wrong!",
+        'message': "You're insane!",
         'likes_count': 0,
         'replies_count': 0,
         'reposts_count': 0,
