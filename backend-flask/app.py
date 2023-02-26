57f5139370ea3d00d65c5a54c2fc85c68a337182
Albert I Palacios <albertpalacios70@yahoo.com>
2023-02-26 22:23:48 +0000

    other commit

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -29,7 +29,8 @@ from aws_xray_sdk.core import xray_recorder
 from aws_xray_sdk.ext.flask.middleware import XRayMiddleware
 
 # cloudwatch
-import watchtower, logging
+import watchtower
+import logging
 from time import strftime
 
 LOGGER = logging.getLogger(__name__)
