bc8698fa48cd5f4407c6f79ef8a739d59dbdb59a
Albert I Palacios <albertpalacios70@yahoo.com>
2023-03-08 22:15:27 +0000

    cognito token verification

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -14,7 +14,6 @@ from services.message_groups import *
 from services.messages import *
 from services.create_message import *
 from services.show_activity import *
-from lib.cognito_jwt_token import CognitoJwtToken
 
 # Honeycomb: Tracing
 from opentelemetry import trace
@@ -68,12 +67,6 @@ trace.set_tracer_provider(provider)
 tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
-
-cognito_jwt_token = CognitoJwtToken(
-   user_pool_id = os.getenv("AWS_COGNITO_USER_POOL_ID"), 
-   user_pool_client_id = os.getenv("AWS_COGNITO_USER_POOL_CLIENT_ID"), 
-   region = os.getenv("AWS_DEFAULT_REGION")
-)
 
 # Rollbar
 rollbar_access_token = os.getenv('ROLLBAR_ACCESS_TOKEN')
@@ -159,16 +152,6 @@ def data_create_message():
 
 @app.route("/api/activities/home", methods=['GET'])
 def data_home():
-  access_token = CognitoJwtToken.extract_access_token(request.headers)
-  try:
-    claims = cognito_jwt_token.token_service.verify(access_token)   
-  except TokenVerifyError as e:
-    _ = request.data
-    abort(make_response(jsonify(message=str(e)), 401))
-
-  app.logger.debug('claims')
-  app.logger.debug(claims)
-
   data = HomeActivities.run(logger=LOGGER)
   return data, 200
 
